592bfd04a9bef653b826fa8bc22a124e60612f16
Genqila <102831561+genqila@users.noreply.github.com>
2026-02-03 16:18:47 +0700

    feat(run): shorten tomb run by going straight to the special room

--- internal/run/tal_rasha_tombs.go
+++ internal/run/tal_rasha_tombs.go
@@ -3,6 +3,7 @@ package run
 import (
 	"github.com/hectorgimenez/d2go/pkg/data"
 	"github.com/hectorgimenez/d2go/pkg/data/area"
+	"github.com/hectorgimenez/d2go/pkg/data/object"
 	"github.com/hectorgimenez/d2go/pkg/data/quest"
 	"github.com/hectorgimenez/d2go/pkg/data/stat"
 	"github.com/hectorgimenez/koolo/internal/action"
@@ -37,17 +38,15 @@ func (a TalRashaTombs) CheckConditions(parameters *RunParameters) SequencerResul
 var talRashaTombs = []area.ID{area.TalRashasTomb1, area.TalRashasTomb2, area.TalRashasTomb3, area.TalRashasTomb4, area.TalRashasTomb5, area.TalRashasTomb6, area.TalRashasTomb7}
 
 func (a TalRashaTombs) Run(parameters *RunParameters) error {
-
-	// Iterate over all Tal Rasha Tombs
+	// Iterate over all Tal Rasha Tombs.
 	for _, tomb := range talRashaTombs {
-
-		// Use the waypoint to travel to Canyon Of The Magi
+		// Travel to the Canyon of the Magi waypoint.
 		err := action.WayPoint(area.CanyonOfTheMagi)
 		if err != nil {
 			return err
 		}
 
-		// Move to the next Tomb
+		// Enter the next tomb.
 		if err = action.MoveToArea(tomb); err != nil {
 			return err
 		}
@@ -58,35 +57,64 @@ func (a TalRashaTombs) Run(parameters *RunParameters) error {
 		// Buff before we start
 		action.Buff()
 
-		a.ctx.CharacterCfg.Character.ClearPathDist = 20
-		if err := config.SaveSupervisorConfig(a.ctx.CharacterCfg.ConfigFolderName, a.ctx.CharacterCfg); err != nil {
-			a.ctx.Logger.Error("Failed to save character configuration: %s", err.Error())
-		}
-
-		shouldInterrupt := func() bool {
-			if parameters != nil && parameters.SequenceSettings != nil && parameters.SequenceSettings.MaxLevel != nil {
-				ctx := context.Get()
-				if lvl, found := ctx.Data.PlayerUnit.FindStat(stat.Level, 0); found {
-					return lvl.Value > *parameters.SequenceSettings.MaxLevel
-				}
+		// Find the Duriel tomb (orifice) or regular tomb (sparkly chest) special room.
+		targetObject := data.Object{}
+		for _, obj := range a.ctx.Data.Objects {
+			if obj.Name == object.HoradricOrifice || obj.Name == object.SparklyChest {
+				targetObject = obj
+				break
 			}
-			return false
 		}
 
-		// Clear the Tomb
-		if err = action.ClearCurrentLevelEx(true, data.MonsterAnyFilter(), shouldInterrupt); err != nil {
-			return err
+		// Clear the Tomb until finding the special room.
+		if targetObject.Name == 0 {
+			a.ctx.Logger.Warn("Tal Rasha Tombs run: special room not found, exploring tomb")
+			if err = action.ClearCurrentLevelEx(true, data.MonsterAnyFilter(), func() bool {
+				// Find the Duriel tomb (orifice) or regular tomb (sparkly chest) special room.
+				for _, obj := range a.ctx.Data.Objects {
+					if obj.Name == object.HoradricOrifice || obj.Name == object.SparklyChest {
+						targetObject = obj
+						break
+					}
+				}
+				// Room found, aborting the clear.
+				if targetObject.Name != 0 {
+					a.ctx.Logger.Warn("Tal Rasha Tombs run: special room found during exploration")
+					return true
+				}
+
+				return false
+			}); err != nil {
+				return err
+			}
+			// Room was not found during the clear.
+			if targetObject.Name == 0 {
+				a.ctx.Logger.Warn("Tal Rasha Tombs run: special room not found after exploration")
+			}
 		}
 
-		// Return to town
+		// Move to the special room.
+		if targetObject.Name != 0 {
+			if err := action.MoveToCoords(targetObject.Position); err != nil {
+				return err
+			}
+		}
+
+		// Return to town before moving to the next tomb.
 		if err = action.ReturnTown(); err != nil {
 			return err
 		}
 
-		if shouldInterrupt() {
-			return nil
+		// Allow early stop for leveling sequences that cap the desired level.
+		if parameters != nil && parameters.SequenceSettings != nil && parameters.SequenceSettings.MaxLevel != nil {
+			ctx := context.Get()
+			if lvl, found := ctx.Data.PlayerUnit.FindStat(stat.Level, 0); found && lvl.Value > *parameters.SequenceSettings.MaxLevel {
+				a.ctx.Logger.Info("Tal Rasha Tombs run: interrupted due to max level reached")
+				return nil
+			}
 		}
 	}
 
+	// All tombs completed.
 	return nil
 }
